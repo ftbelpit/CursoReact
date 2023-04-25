db3df67ed3a714d7df016dcfdc7b15b4518ebc98
ftbelpit <ftristante@belpit.com.br>
2023-04-25 16:00:12 -0300

    *

--- 5_FORM_EM_REACT/form/src/App.js
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import MyForm from './components/MyForm';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm/>
+      <MyForm user={{ name: "Felipe", email:"felipe@gmail.com", bio: "Sou um programador", role: "admin" }}/>
     </div>
   );
 }
