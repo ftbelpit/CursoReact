88d773dac8c9083baa80beb440ee32141bc0ef10
ftbelpit <ftristante@belpit.com.br>
2023-05-03 16:01:26 -0300

    *

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore} from "firebase/firebase"
+import { getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmbEMIirfX_S8rICC_42mjHfRlDrap5EM",
--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config"
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -24,5 +26,90 @@ export const useAuthentication = () => {
         }
     }
 
+    //register
+    const createUser = async (data) => {
+        checkIfIsCancelled()
 
+        setLoading(true)
+        setError(null)
+
+        try {
+            const {user} = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password,
+            )
+
+            await updateProfile(user, {
+                displayName: data.displayName
+            })
+
+            setLoading(false)
+
+            return user
+        } catch (error) {
+            console.log(error.message)
+            console.log(typeof error.message)
+
+            let systemErrorMessage
+
+            if(error.message.includes("Password")) {
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres"
+            } else if(error.message.includes("email-already")) {
+                systemErrorMessage = "E-mail já cadastrado."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde."
+            }
+
+            setLoading(false)
+            setError(systemErrorMessage)
+        }
+
+    }
+
+    // logout - sign out
+    const logout = () => {
+        checkIfIsCancelled()
+    
+        signOut(auth)
+    }
+
+    // login - sign in
+    const login = async (data) => {
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+        } catch (error) {
+            let systemErrorMessage
+
+            if(error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado."
+            } else if(error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente novamente mais tarde."
+            }
+
+            setLoading(false)
+            setError(systemErrorMessage)
+        }
+    }
+
+    useEffect(() => {
+        return() => setCancelled(true)
+    }, [])
+
+    return {
+        auth,
+        createUser,
+        error,
+        loading,
+        logout,
+        login
+    }
 }
